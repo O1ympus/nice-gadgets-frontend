6dc4dcddbdb9b687fd59ba2caa9a2cae9bebf169
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-09-15 14:46:37 +0300

    added right button with all possible states

---
+++ src/components/atoms/buttons/SliderButtonRight.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { useRef } from 'react';
+import cn from 'classnames';
+
+type SliderButtonRightProps = {
+  onClick: () => void;
+  disabled: boolean;
+};
+
+export const SliderButtonRight: React.FC<SliderButtonRightProps> = ({
+  onClick,
+  disabled,
+}) => {
+  const state = useRef(null);
+  return (
+    <button
+      ref={state}
+      onClick={onClick}
+      disabled={disabled}
+      className="
+        flex items-center justify-center
+        w-[36px] h-[36px]
+        cursor-pointer
+        bg-transparent
+        box-border
+        border-solid
+        border border-elements
+        transition-all duration-200
+        hover:border-custom-primary
+        disabled:border-elements
+      "
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="6"
+        height="10"
+        viewBox="0 0 6 10"
+        fill="none"
+        className={cn({
+          'fill-primary': !disabled,
+          'fill-elements': disabled,
+        })}
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M0.528758 0.528606C0.789108 0.268256 1.21122 0.268256 1.47157 0.528606L5.47157 4.52861C5.73192 4.78896 5.73192 5.21107 5.47157 5.47141L1.47157 9.47141C1.21122 9.73176 0.789108 9.73176 0.528758 9.47141C0.268409 9.21107 0.268409 8.78896 0.528758 8.52861L4.05735 5.00001L0.528758 1.47141C0.268409 1.21107 0.268409 0.788955 0.528758 0.528606Z"
+          fill="primary"
+        />
+      </svg>
+    </button>
+  );
+};
